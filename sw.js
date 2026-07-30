eff0af196de6ac35eda2c6ce5c953af4b2adb68d
tritornpattra-crypto <tritornpat.tra@student.sk.ac.th>
2026-07-30 21:27:28 +0700

    Initial commit: Shot Log PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+// แคชไฟล์แอปไว้ให้เปิดใช้ได้แม้ไม่มีเน็ต
+const CACHE = "shotlog-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (res.ok && e.request.url.startsWith(self.location.origin)) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
